d88cbc4c39561fd398a866ef963933f5142b192e
michaelchen <mchen7588@gmail.com>
2023-06-01 16:33:19 -0700

    add header styling

--- src/components/LayoutProvider/LayoutProvider.tsx
+++ src/components/LayoutProvider/LayoutProvider.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import "./LayoutProvider.css";
 
 function LayoutProvider(props: React.PropsWithChildren<{}>) {
+  const location = useLocation();
+
   return (
     <div className="app-container">
       <header className="header">
-      <Link to="/">
-        Home
-      </Link>
-      <Link to="/teams">
-        Teams
-      </Link>
-      <Link to="/members">
-        Members
-      </Link>
-      <Link to="/todo">
-        ToDo
-      </Link>
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          Home
+        </Link>
+        <Link to="/teams" className={location.pathname === '/teams' ? 'active' : ''}>
+          Teams
+        </Link>
+        <Link to="/members" className={location.pathname === '/members' ? 'active' : ''}>
+          Members
+        </Link>
+        <Link to="/todo" className={location.pathname === '/todo' ? 'active' : ''}>
+          ToDo
+        </Link>
       </header>
       <div className="app-content">
         {props.children}
