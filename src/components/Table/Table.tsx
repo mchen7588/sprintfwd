1ac1f0a1df2abbc7b1e84d88eeca0d5bbc1dd53b
michaelchen <mchen7588@gmail.com>
2023-06-01 15:59:52 -0700

    add table styling

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,10 @@
+import "./Table.css";
+
 function TableRow(props: {rowData: string[]}) {
   return (
-    <tr>
+    <tr className="table-row">
       {
-        props.rowData.map((rowItemData: string) => <td>{rowItemData}</td>)
+        props.rowData.map((rowItemData: string, index: number) => <td key={index} className="table-row-item">{rowItemData}</td>)
       }
     </tr>
   )
@@ -15,17 +17,17 @@ type TableProps = {
 
 function Table(props: TableProps) {
   return (
-    <table>
+    <table className="table-wrapper">
       <thead>
-        <tr>
+        <tr className="table-header">
           {
-            props.headerData.map((headerItemData: string) => <th>{headerItemData}</th>)
+            props.headerData.map((headerItemData: string, index: number) => <th key={index} className="table-header-item">{headerItemData}</th>)
           }
         </tr>
       </thead>
       <tbody>
         {
-          props.rowDataList.map((rowData: string[]) => <TableRow rowData={rowData} />)
+          props.rowDataList.map((rowData: string[], index: number) => <TableRow key={index} rowData={rowData} />)
         }
       </tbody>
     </table>
